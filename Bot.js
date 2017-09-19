ad74fd2a4466a2d5eb6b914f49d9e087786f646b
Dingodhole <lukas.luotola@windowslive.com>
2017-09-19 10:45:54 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -21,65 +21,61 @@ var monthNames = [
     "Marraskuuta", "Joulukuuta"
   ];
 
-//Schedules, updates every day at 8:15AM. Fetches JSON file from Arkea website and extracts the information. Prints corresponding information for each day.
-var j = schedule.scheduleJob('15 8 * * *', function(){
+
+//Get the menu from arkea site
+function getMenu(n) {
+	fetch('https://www.arkea.fi/fi/ruokalista/43/lista')
+  	.then(function(response) {
+    	if (response.status >= 400) {
+      	bot.sendMessage({
+        	to: channelID,
+          message: "Network error occured! I probably just crashed and went offline :( Fuck arkea servers"
+        });
+			}
+       return response.json();
+    })
+    .then(function(menu) {  
+    	var month = parseInt(menu.MenusForDays[number].Date.substring(5,7));
+      var day = parseInt(menu.MenusForDays[number].Date.substring(8,10));
+      var date = day.toString() + '. ' + monthNames[month-1] + " \n";
+
+      var message = "```css\n" + date + "Lounas: \n";
+
+      if (menu.MenusForDays[number].SetMenus.hasOwnProperty("Lounas")) {
+
+      	for (let item in menu.MenusForDays[number].SetMenus.Lounas.Components.Dish) {
+        	if (menu.MenusForDays[number].SetMenus.Lounas.Components.Dish[item] != "Uunimakkara (M, L, G, K)")
+        		message += "   " + menu.MenusForDays[number].SetMenus.Lounas.Components.Dish[item] + "\n";
+          else
+          	message += "   UUUUUUUUUUUUUUUNIMAKKKARAAAAAAAAAAAA BOIIIIIIIIIIIIIIII YEAHHHHHHHHHHHH BOIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII \n";
+        }
+			}
+
+			if (menu.MenusForDays[number].SetMenus.hasOwnProperty("Kasvislounas")) {
+
+      	message += " ``` ```fix\n Kasvislounas: \n"
+
+        for (let item in menu.MenusForDays[number].SetMenus.Kasvislounas.Components.Dish) {
+          message += "   " + menu.MenusForDays[number].SetMenus.Kasvislounas.Components.Dish[item] + "\n";
+        } 
+    	}
+                            
+    	message += "```";
+
+    	bot.sendMessage({
+      	to: channelID,
+    		message: message
+  		});
+	}); 
+}
+
+//Schedules, updates every day at 7:00AM. Fetches JSON file from Arkea website and extracts the information. Prints corresponding information for each day.
+var j = schedule.scheduleJob('7 * * *', function(){
 	var d = new Date();
 	var n = d.getDay()-1;
-
-		fetch('https://www.arkea.fi/fi/ruokalista/43/lista')
-			.then(function(response) {
-				if (response.status >= 400) {
-					bot.sendMessage({
-						to: auth.channelID,
-						message: reponses.timeout
-					});
-				}
-				return response.json();
-			})
-			.then(function(menu) {  
-				try {
-					var month = parseInt(menu.MenusForDays[n].Date.substring(5,7));
-					var day = parseInt(menu.MenusForDays[n].Date.substring(8,10));
-					var date = day.toString() + '. ' + monthNames[month-1] + " \n";
-
-					var message = "```css\n" + date + "Lounas: \n";
-
-					if (menu.MenusForDays[n].SetMenus.hasOwnProperty("Lounas")) {
-
-					   for (let item in menu.MenusForDays[n].SetMenus.Lounas.Components.Dish) {
-							message += "   " + menu.MenusForDays[n].SetMenus.Lounas.Components.Dish[item] + "\n";
-						}
-					}
-
-					if (menu.MenusForDays[n].SetMenus.hasOwnProperty("Kasvislounas")) {
-
-					   message += " ``` ```fix\n Kasvislounas: \n"
-
-						for (let item in menu.MenusForDays[n].SetMenus.Kasvislounas.Components.Dish) {
-							message += "   " + menu.MenusForDays[n].SetMenus.Kasvislounas.Components.Dish[item] + "\n";
-						} 
-					}
-					
-					message += "```";
-
-					bot.sendMessage({
-						to: auth.channelID,
-						message: message
-					});
-				}
-				
-				catch(e) {
-					//Error occured
-					console.log("Error occured: " + e);
-					bot.sendMessage({
-						to: auth.channelID,
-						message: responses.nomenu
-					});
-				}
-				
-			});
-			
-			console.log('Ruokalista toimitettu');
+	
+  getMenu(n);
+	
 	});
  
 //Informs successful login to the console
@@ -89,17 +85,11 @@ bot.on('ready', function (evt) {
     console.log(bot.username + ' - (' + bot.id + ')');
 });
 
-// Automatically reconnect if the bot disconnects due to inactivity
-bot.on('disconnect', function(erMsg, code) {
-    console.log('----- Bot disconnected from Discord with code', code, 'for reason:', erMsg, '-----');
-    bot.connect();
-});
-
 //How bot will act on incoming messages.
 bot.on('message', function (user, userID, channelID, message, evt) {
     
     if (message.substring(0, 1) == settings.prefix) {
-	message = message.toLowerCase();
+        message = message.toLowerCase();
         var args = message.substring(1).split(' ');
         var cmd = args[0];
 		msg = message;
@@ -168,7 +158,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'menu':
                 var number = 20;
 
-                var argNumber = args[1];
+                var argNumber = args[1].toLowerCase();
 
                 paiva:
                 switch(argNumber) {
@@ -197,49 +187,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
 
                 if (number < 5 && number >= 0){
-                    fetch('https://www.arkea.fi/fi/ruokalista/43/lista')
-                        .then(function(response) {
-                            if (response.status >= 400) {
-                                bot.sendMessage({
-                                    to: channelID,
-                                    message: "Network error occured! I probably just crashed and went offline :( Fuck arkea servers"
-                                });
-                            }
-                            return response.json();
-                        })
-                        .then(function(menu) {  
-                            var month = parseInt(menu.MenusForDays[number].Date.substring(5,7));
-                            var day = parseInt(menu.MenusForDays[number].Date.substring(8,10));
-                            var date = day.toString() + '. ' + monthNames[month-1] + " \n";
-
-                            var message = "```css\n" + date + "Lounas: \n";
-
-                            if (menu.MenusForDays[number].SetMenus.hasOwnProperty("Lounas")) {
-
-                               for (let item in menu.MenusForDays[number].SetMenus.Lounas.Components.Dish) {
-                                    if (menu.MenusForDays[number].SetMenus.Lounas.Components.Dish[item] != "Uunimakkara (M, L, G, K)")
-                                        message += "   " + menu.MenusForDays[number].SetMenus.Lounas.Components.Dish[item] + "\n";
-                                    else
-                                        message += "   UUUUUUUUUUUUUUUNIMAKKKARAAAAAAAAAAAA BOIIIIIIIIIIIIIIII YEAHHHHHHHHHHHH BOIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII \n";
-                                }
-                            }
-
-                            if (menu.MenusForDays[number].SetMenus.hasOwnProperty("Kasvislounas")) {
-
-                               message += " ``` ```fix\n Kasvislounas: \n"
-
-                                for (let item in menu.MenusForDays[number].SetMenus.Kasvislounas.Components.Dish) {
-                                    message += "   " + menu.MenusForDays[number].SetMenus.Kasvislounas.Components.Dish[item] + "\n";
-                                } 
-                            }
-                            
-                            message += "```";
-
-                            bot.sendMessage({
-                                to: channelID,
-                                message: message
-                            });
-                        }); 
+                    getMenu(number);
                 } else {
                     bot.sendMessage({
                         to: channelID,
