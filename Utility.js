40a578675110df86870940d226e9388ecff01c3d
Dingodhole <lukas.luotola@windowslive.com>
2017-12-21 11:50:20 +0200

    New requirement for Program.js

---
+++ Utility.js
@@ -0,0 +1,29 @@
+//Utility module
+
+module.exports = {
+	returnThisDay: function() {
+		var d = new Date();
+		var month = d.getMonth()+1;
+		var year = d.getFullYear();
+		var day = d.getDate();
+
+		var Today = (year + "-" + month + "-" + day + "T00:00:00");
+		
+		return Today;
+	},
+	saveMessage: function(author, reaction) {
+		author.send({
+			embed: {
+				"color": 0x5a5a5a,
+				"timestamp": new Date(),
+				"fields": [{
+					"name": reaction.message.author.username + " in " + reaction.message.guild + ", #" + reaction.message.channel.name,
+					"value": reaction.message.content
+				}]
+			}
+		});
+		reaction.remove(author).catch((e) => {
+			reaction.message.channel.send("Can't delete reaction. " + e);
+		})
+	}
+}
