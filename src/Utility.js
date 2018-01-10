7fadb9b28f31b05f89333d640ca4f7f6ef9bb7b5
Dingodhole <lukas.luotola@windowslive.com>
2018-01-10 17:30:15 +0200

    It just works

--- src/Utility.js
+++ src/Utility.js
@@ -2,7 +2,7 @@
 
 function returnThisDay() {
 	let d = new Date();
-	
+
 	let day = ("0" + d.getDate()).slice(-2);
 	let month = ("0" + (d.getMonth() + 1)).slice(-2);
 	let year = d.getFullYear();
@@ -14,8 +14,8 @@ function returnThisDay() {
 
 function ConvertToISO(shortdate) {
 	let sd = shortdate.split(/[.,/]/);
-	let day = ("0" + sd[0]).slice(-2);
-	let month = ("0" + sd[1]).slice(-2);
+	let day = ("0" + parseInt(sd[0]).toString()).slice(-2);
+	let month = ("0" + sd[1].toString()).slice(-2);
 	let year = sd[2];
 
 	let ISO8601 = (year + "-" + month + "-" + day + "T00:00:00");
