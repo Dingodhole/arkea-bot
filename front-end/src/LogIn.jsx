c195ae60b754d123e0fef79871a76122d359202d
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2018-01-17 10:42:48 +0200

    Added punch of API requests

--- front-end/src/LogIn.jsx
+++ front-end/src/LogIn.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { parse } from 'qs'
 
+import { CircularProgress } from 'material-ui/Progress'
+
 import info from './info.json'
 
 class App extends Component {
@@ -12,12 +14,13 @@ class App extends Component {
 
     this.state = {
       code,
-      token: ""
+      loading: true,
+      id: "",
+      rolesList: []
     }
   }
 
   async componentDidMount() {
-    console.log(this.state.code.code);
     try {
       let req = await fetch(`https://discordapp.com/api/oauth2/token?client_id=${info.id}&client_secret=${info.secret}&grant_type=authorization_code&code=${this.state.code.code}&redirect_uri=${encodeURI(info.URL)}`,
                 {
@@ -27,9 +30,54 @@ class App extends Component {
                   }
                 })
       let json = await req.json()
-      console.log(json)
+      let token = json.access_token
+      let user = await fetch(`https://discordapp.com/api/users/@me`,
+                {
+                  headers: {
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                  }
+                })
+      let userJson = await user.json()
+      let id = userJson.id
+      let memberRolesRaw = await fetch(`https://discordapp.com/api/guilds/${info.guild}/members/${id}`,
+                {
+                  headers: {
+                    'Authorization': 'Bot ' + info.token,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                  }
+                }).then(obj => (obj.json()))
+      let memberRoles = memberRolesRaw.roles
+      let guildRoles = await fetch(`https://discordapp.com/api/guilds/${info.guild}/roles`,
+                {
+                  headers: {
+                    'Authorization': 'Bot ' + info.token,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                  }
+                }).then(obj => (obj.json()))
+
+      let rolesList = []
+
+      guildRoles.forEach(role => {
+        if(memberRoles.includes(role.id) && role.color === 15) {
+          rolesList.push({
+            has: true,
+            name: role.name,
+            id: role.id
+          })
+        } else if (role.color === 15){
+          rolesList.push({
+            has: false,
+            name: role.name,
+            id: role.id
+          })
+        }
+      })
+
       this.setState({
-        token: json.access_token
+        loading: false,
+        id,
+        rolesList
       })
     } catch(e) {
       console.log(e)
@@ -37,9 +85,20 @@ class App extends Component {
   }
 
   render() {
+
+    const content = this.state.loading ?
+      <CircularProgress />
+    :
+      <div>
+        <p>{this.state.id}</p>
+        {this.state.rolesList.map(item => (
+          <p className={item.has ? "has" : "not"}>{item.name}</p>
+        ))}
+      </div>
+
     return (
       <div className="App">
-        <h1></h1>
+        {content}
       </div>
     );
   }
