a385040bbdd4f3baaa4b54ab49d8a31e0d2ee1d3
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2018-01-24 15:03:30 +0200

    Changed test file

--- src/test.js
+++ src/test.js
@@ -3,7 +3,7 @@ require('isomorphic-fetch');
 fetch(`https://discordapp.com/api/channels/390464644810276870/messages/`,
   {
     headers: {
-      'Authorization': 'Bot NDA1NjkzMjcxNjc5OTU5MDUw.DUoG1Q.T3CM6mCYlLWDIyhaGDS-XbQAsd0',
+      'Authorization': 'Bot ',
     }
   }).then(function (res) {
     return(
