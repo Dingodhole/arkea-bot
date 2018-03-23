1d9ffdae5867e29509a328175173c07df933225b
Dingodhole <lukas.luotola@windowslive.com>
2018-03-23 13:20:23 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Bot from './Bot';
+
+const bot = new Bot();
+
+bot.start();
+
+process.on('uncaughtException', (error) => {
+    // eslint-disable-next-line no-console
+    console.error('uncaughtException', error || '');
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, p) => {
+    // eslint-disable-next-line no-console
+    console.error('unhandledRejection', reason || '', p || '');
+});
