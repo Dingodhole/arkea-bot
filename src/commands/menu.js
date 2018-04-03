48bacc07685a393641313589c314d627f0018420
Dingodhole <lukas.luotola@windowslive.com>
2018-04-03 20:28:55 +0300

    Added menu command

---
+++ src/commands/menu.js
@@ -0,0 +1,25 @@
+import config from '../../config/config.json'
+import {getMenu, SetCWeekMenuURL} from '../functions';
+
+const cmd = async (id = config.restaurantID, day = new Date()) => {
+    let result = await SetCWeekMenuURL(id, day);
+	await getMenu(result, day, message.channel);
+}
+
+// Command help. This will be shown when user prompts <prefix>help <cmd_name>
+cmd.help = {
+    name: "menu",
+    category: "Arkea Essentials",
+    description: "Get menu",
+    usage: "menu"
+}
+
+// Command configuration
+cmd.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "everyone"
+}
+
+export default cmd
