6a8cc54c760bd953747e9a45b3777b4ad31307cf
Dingodhole <lukas.luotola@windowslive.com>
2018-04-03 22:20:50 +0300

    Added documentation

--- src/commands/menu.js
+++ src/commands/menu.js
@@ -1,20 +1,40 @@
+"use strict"
+
 import config from '../../config/config.json'
 import { getMenu, SetCWeekMenuURL } from '../functions';
 
+/**
+* Fetches correct menu for given restaurant at given date.
+* @param {string} [id = config.restaurantID] - Restaurant ID. Defaults to guild's restaurant ID.
+* @param {Date} [day = new Date()] - Sets day. Defaults to today.
+*/
 const cmd = async (id = config.restaurantID, day = new Date()) => {
   let result = await SetCWeekMenuURL(id, day);
   await getMenu(result, day, message.channel);
 }
 
-// Command help. This will be shown when user prompts <prefix>help <cmd_name>
+/**
+* Command information
+* Displays information about the command and how to use it.
+* @prop {string} name - Command name
+* @prop {string} category - Command category
+* @prop {string} description - Command description
+* @prop {string} usage - How to use the command. <cmd_name> (<params>)
+*/
 cmd.info = {
   name: "menu",
   category: "Arkea Essentials",
-  description: "Get menu",
-  usage: "menu <day>"
+  description: "Fetches menu for given date. You can use your own restauran ID and day. Defaults to your guild's default restaurant ID and date is set for today.",
+  usage: "menu [-Id <restaurantID>] [-d <day>]"
 }
 
-// Command configuration
+/**
+* Command configuration
+* @prop {boolean} enabled - Command is enabled/disabled
+* @prop {boolean} guildOnly - Should this command be usable only from guild chat
+* @prop {string} aliases - List of possible command aliases
+* @prop {string} permLevel - Set command permissions level
+*/
 cmd.conf = {
   enabled: true,
   guildOnly: false,
