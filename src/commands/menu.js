0f00decaee94e5d1e0bf4f65559ed5518db5505f
Dingodhole <lukas.luotola@windowslive.com>
2018-04-03 20:41:32 +0300

    Changed help -> info. Fixed indentations.

--- src/commands/menu.js
+++ src/commands/menu.js
@@ -3,23 +3,23 @@ import { getMenu, SetCWeekMenuURL } from '../functions';
 
 const cmd = async (id = config.restaurantID, day = new Date()) => {
   let result = await SetCWeekMenuURL(id, day);
-	await getMenu(result, day, message.channel);
+  await getMenu(result, day, message.channel);
 }
 
 // Command help. This will be shown when user prompts <prefix>help <cmd_name>
-cmd.help = {
-    name: "menu",
-    category: "Arkea Essentials",
-    description: "Get menu",
-    usage: "menu"
+cmd.info = {
+  name: "menu",
+  category: "Arkea Essentials",
+  description: "Get menu",
+  usage: "menu <day>"
 }
 
 // Command configuration
 cmd.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    permLevel: "everyone"
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "everyone"
 }
 
 export default cmd
