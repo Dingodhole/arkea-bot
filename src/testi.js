cff1ca4f55dc4fffd9f8c9a02c173bda29701556
Dingodhole <lukas.luotola@windowslive.com>
2018-04-05 17:02:46 +0300

    Removed unnecessary files

--- src/testi.js
+++
@@ -1,5 +0,0 @@
-let date1 = new Date();
-let date2 = new Date("2018-06-02T12:00:00+02:00");
-let timeDiff = Math.abs(date2.getTime() - date1.getTime());
-let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-console.log(diffDays-1)
